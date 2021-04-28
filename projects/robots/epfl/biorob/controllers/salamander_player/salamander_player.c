953b563781c672a45b13eb0ed9dda27d81beeee8
utaro <utaro.matsuura@gmail.com>
2021-04-28 14:41:17 +0900

    score

--- projects/robots/epfl/biorob/controllers/salamander_player/salamander_player.c
+++ projects/robots/epfl/biorob/controllers/salamander_player/salamander_player.c
@@ -52,7 +52,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <webots/keyboard.h>
 #include <webots/motor.h>
 #include <webots/robot.h>
-
+#include <stdlib.h>
 /* for array indexing */
 #define X 0
 #define Y 1
@@ -66,6 +66,21 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 /* virtual time between two calls to the run() function */
 #define CONTROL_STEP 32
+
+#include <webots/receiver.h>
+/*
+ * The following macros are useful for retrieving the x,y coordinates and
+ * angles value for each robot specified by a team id: 'y' or 'b' and a
+ * player id: '1', '2', or '3'
+ */
+#define ROBOTS 6
+#define robot_get_id(t, p) (3 * ((p) - '1' + (((t) == 'y') ? ROBOTS / 2 : 0)))
+#define robot_get_x(t, p) packet[robot_get_id(t, p)]
+#define robot_get_y(t, p) packet[robot_get_id(t, p) + 1]
+#define robot_get_orientation(t, p) packet[robot_get_id(t, p) + 2]
+#define ball_get_x() packet[ROBOTS * 3]
+#define ball_get_y() packet[ROBOTS * 3 + 1]
+
 
 /* global variables */
 static double spine_offset = 0.0;
@@ -139,15 +154,29 @@ void read_keyboard_command() {
 }
 
 int main() {
+
+  WbDeviceTag receiver; /* to receive coordinate information */
+  WbDeviceTag left_motor, right_motor;
+  char team;   /* can be either 'y' for yellow or 'b' for blue */
+  char player; /* can be either '1', '2' or '3' */
+  const char *name;
+  int counter = 0, max = 50;
+  double y, d;
+
+  wb_robot_init();
+  name = wb_robot_get_name();
+  team = name[0];
+  player = name[1];
+  receiver = wb_robot_get_device("receiver");
+
+  
   const double FREQUENCY = 1.4; /* locomotion frequency [Hz] */
   const double WALK_AMPL = 0.6; /* radians */
   const double SWIM_AMPL = 1.0; /* radians */
 
   /* body and leg motors */
   WbDeviceTag motor[NUM_MOTORS];
-  /* double target_position[NUM_MOTORS] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0}; */
-  /* 0428 2 */
-  double target_position[NUM_MOTORS] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, M_PI, M_PI, M_PI, M_PI};  
+  double target_position[NUM_MOTORS] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
   /* distance sensors and gps devices */
   WbDeviceTag ds_left, ds_right, gps;
@@ -160,7 +189,7 @@ int main() {
 
   /* get the motors device tags */
   const char *MOTOR_NAMES[NUM_MOTORS] = {"motor_1", "motor_2",     "motor_3",     "motor_4",     "motor_5",
-                                         "motor_6", "motor_leg_1", "motor_leg_2", "motor_leg_3", "motor_leg_4"}; /* 0428 1 */
+                                         "motor_6", "motor_leg_1", "motor_leg_2", "motor_leg_3", "motor_leg_4"};
   for (i = 0; i < NUM_MOTORS; i++) {
     motor[i] = wb_robot_get_device(MOTOR_NAMES[i]);
     min_motor_position[i] = wb_motor_get_min_position(motor[i]);
@@ -191,6 +220,63 @@ int main() {
 
   /* control loop: sense-compute-act */
   while (wb_robot_step(CONTROL_STEP) != -1) {
+
+    int left_speed = 0;
+    int right_speed = 0;
+
+    while (wb_receiver_get_queue_length(receiver) > 0) {
+      const double *packet = wb_receiver_get_data(receiver);
+      switch (player) {
+        /* case '1': /\* first player *\/ */
+        case '1': /* first player */        
+          /* falls through */
+          y = robot_get_y(team, player); /* my own y coordinate */
+          printf("teamA1 = ");
+          /* printf(y); */
+        case '2': /* second player */
+          /*
+           * This is a very stupid random behavior, up to you to improve it!
+           * Advice: compare the ball coordinates to your own coordinates to
+           * decide how to move the robot.
+           */
+          counter++;
+          if (counter < max) {
+            left_speed = 10;
+            right_speed = 10;
+          } else {
+            left_speed = -10;
+            right_speed = 10;
+          }
+          if (counter == 100) {
+            counter = 0; /* reset the counter */
+
+            /* Random number between 30 and 99 */
+            max = 30 + rand() / (RAND_MAX / 70);
+          }
+          break;
+        case '3': /* goal keeper */
+          /*
+           * Here, we make the goal keeper follow the y coordinate of the ball
+           * while remaining inside the goal width with y inside [-0.16;+0.16]
+           * This is very simple and doesn't guarantee the goal keeper will
+           * remain in the goal for ever...
+           */
+          y = robot_get_y(team, player); /* my own y coordinate */
+          d = ball_get_y() - y;
+
+          if (d > 0.01 && y < 0.16) {
+            right_speed = 10;
+            left_speed = 10;
+          } else if (d < -0.01 && y > -0.16) {
+            right_speed = -10;
+            left_speed = -10;
+          }
+          break;
+      }
+
+      wb_receiver_next_packet(receiver);
+    }
+    
     read_keyboard_command();
 
     if (control == AUTO) {
@@ -229,19 +315,10 @@ int main() {
           target_position[i] = WALK_AMPL * ampl * A[i] * sin(phase) + spine_offset;
 
         /* rotate legs */
-        /* target_position[6] = phase; */
-        /* target_position[7] = phase + M_PI; */
-        /* target_position[8] = phase + M_PI; */
-        /* target_position[9] = phase; */
-        /* 0428 2 */
-        /* target_position[6] = M_PI; */
-        /* target_position[7] =  M_PI; */
-        /* target_position[8] =  M_PI; */
-        /* target_position[9] = M_PI; */
-        target_position[6] = M_PI;
-        target_position[7] =  M_PI;
-        target_position[8] =  M_PI;
-        target_position[9] = M_PI;        
+        target_position[6] = phase;
+        target_position[7] = phase + M_PI;
+        target_position[8] = phase + M_PI;
+        target_position[9] = phase;
       } else { /* SWIM */
         /* below water level: swim (travelling wave of robot body) */
         for (i = 0; i < 6; i++)
